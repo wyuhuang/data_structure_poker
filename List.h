61b5a918d32acd6ab62b52fda321d6ed898e8259
wyuhuang <44691615+wyuhuang@users.noreply.github.com>
2020-12-03 18:58:23 +0800

    修复“LinkedList.insert”插入失败bug
    重构，将insert方法改成add方法，增加了tail字段，实现O（1）复杂度尾部插入

--- List.h
+++ List.h
@@ -8,7 +8,7 @@ class List {
         virtual int getLength() = 0;
         virtual int locate(T data) = 0;
         virtual T get(int index) = 0;
-        virtual void insert(T data, int index) = 0;
+        virtual void add(T data) = 0;
         virtual T deleteByIndex(int index) = 0;
         virtual bool isEmpty() = 0;
         virtual void printList() = 0;
